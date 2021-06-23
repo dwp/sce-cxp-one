6de6471b4976fa4d061a2c2cad41bd199a111c25
Sarat K <sarat.kumar@engineering.digital.dwp.gov.uk>
2021-06-23 09:48:21 +0100

    updates 1st push

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
+
+// Branching your-contact-channels
+router.post('/e-one/your-contact-channels', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const contactChannel = req.session.data['contact-channel']
+
+  if (contactChannel === 'telephone') {
+    res.redirect('/e-one/telephone-information')
+  }
+
+  else if (contactChannel === 'dss-signin') {
+    res.redirect('/e-one/sign-in')
+  }
+
+  else if (contactChannel === 'chat-bots') {
+    res.redirect('/e-one/webchat-service')
+  }
+
+})
