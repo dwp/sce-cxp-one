b2f75c407918687b814c70231b0dbd77300f19b0
Sarat K <sarat.kumar@engineering.digital.dwp.gov.uk>
2021-07-05 08:47:25 +0100

    Added Exp;2

--- app/routes.js
+++ app/routes.js
@@ -51,3 +51,50 @@ router.post('/e-one/contact-channels', function (req, res) {
 
 
 })
+
+// Branching Experiement 2 Information-channels
+router.post('/e-two/information-channels', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const contactChannel = req.session.data['contact-channel']
+
+  if (contactChannel === 'telephone') {
+    res.redirect('/e-two/telephone-information')
+  }
+
+  else if (contactChannel === 'dss-signin') {
+    res.redirect('/e-two/benefit-summary')
+  }
+
+  else if (contactChannel === 'chat-bots') {
+    res.redirect('/e-two/webchat-service')
+  }
+
+    else if (contactChannel === 'no-i-did-not-found-info') {
+      res.redirect('/e-two/did-you-get-all-info-no')
+    }
+    else if (contactChannel === 'no-i-do-not') {
+      res.redirect('/e-two/thank-you-close')
+    }
+
+})
+
+router.post('/e-two/contact-channels', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const contactChannel = req.session.data['contact']
+
+  if (contactChannel === 'webchat') {
+    res.redirect('/e-two/webchat-service')
+  }
+
+  else  {
+    res.redirect('/e-two/thank-you')
+  }
+
+
+})
